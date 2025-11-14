6b4fdad004a0cfe577de8b1a57a5e349bf42ee68
Aflah Riswan <riswanaflah@gmail.com>
2025-11-14 17:39:47 +0530

    tracked all files

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-
-const express= require('express')
-const cors=require('cors')
-const app=express()
+require("dotenv").config();
+const express = require('express')
+const cors = require('cors');
+const { connectDB } = require('./src/config/db');
+const products = require('./models/products'); 
+const app = express()
+connectDB()
 app.use(cors());
-app.get('/',(req,res)=>{
-  res.send({message:'hello world'})
+app.get('/', (req, res) => {
+  res.send({ message: 'hello world' })
 })
-app.listen(3000,()=>console.log('server is running..'))
+app.get('/products', async (req, res) => {
+  try {
+    const items= await products.find()
+    console.log(items)
+    res.json({status:'success',products:items})
+  } catch (error) {
+    res.json({status:'failed',message:error})
+  }
+  
+})
+app.listen(3000, () => console.log('server is running..'))
---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose')
+
+ const connectDB = async () =>{
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log(" DATABASE CONNECTED SUCCESSFULLY ")
+  } catch (error) {
+    console.log(" GOT ERROR IN  DATABASE CONECTION : ",error)
+  }
+}
+module.exports = {connectDB}
