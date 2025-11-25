85e16daedce8a4f01d921ca28c556431640b3d1d
Aflah Riswan <riswanaflah@gmail.com>
2025-11-25 17:24:47 +0530

    saving before starting the admin-login

---
+++ controllers/authController.js
@@ -0,0 +1,4 @@
+
+const User= require('../models/users')
+const bcrypt= require('bcrypt')
+const jwt= require('')
--- index.js
+++ index.js
@@ -2,21 +2,11 @@ require("dotenv").config();
 const express = require('express')
 const cors = require('cors');
 const { connectDB } = require('./src/config/db');
-const products = require('./models/products'); 
+const users=require('./models/users')
 const app = express()
 connectDB()
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send({ message: 'hello world' })
-})
-app.get('/products', async (req, res) => {
-  try {
-    const items= await products.find()
-    console.log(items)
-    res.json({status:'success',products:items})
-  } catch (error) {
-    res.json({status:'failed',message:error})
-  }
-  
-})
+app.use(express.json())
+
+
 app.listen(3000, () => console.log('server is running..'))
